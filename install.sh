98727302dc3aefe9dcc46bad7fcd73456f354a29
sakaki <91630610+sakaki91@users.noreply.github.com>
2026-03-23 14:55:05 -0300

    Update 3.1-3_tmp
    Modification and generation of logs in `~/` is a temporary version and will be improved soon.

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ creationTree(){
 
 variableTree(){
     [ -d "$TREE" ] && rm -rf "$TREE"
+    [ -f "$AR_LOG" ] && rm -rf "$AR_LOG"
     PROGRAM=${TREE}/PROGRAM
     PREFIX=${TREE}/PREFIX
     RUNNER=${TREE}/RUNNER
@@ -49,34 +50,35 @@ runner(){
 dependencyInstall(){
     clear
     export WINEPREFIX=${PREFIX}
-    echo -e "$WAIT_LOG Proton Wineboot." && $RUNNER_EXEC wineboot -u &> /dev/null
+    echo -e "$WAIT_LOG Proton Wineboot." && $RUNNER_EXEC wineboot -u &> $AR_LOG
         [ -d "$PREFIX"/pfx ] && echo -e " $DONE_LOG Structure created!"
         [ ! -d "$PREFIX"/pfx ] && echo -e " $ERROR_LOG Structure not created." && exit
     echo -e "$WAIT_LOG Downloading .NET Runtime.\n$WAIT_LOG Downloading .NET Desktop Runtime."
-    wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &> /dev/null
-    wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP" &> /dev/null
+    wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AR_LOG
+    wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AR_LOG
         [ -f "$TMP"/dotnet-runtime-win-x64.exe ] && echo -e " $DONE_LOG .NET Runtime was downloaded!"
         [ ! -f "$TMP"/dotnet-runtime-win-x64.exe ] && echo -e " $ERROR_LOG .NET Runtime was not downloaded." && exit
         [ -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && echo -e " $DONE_LOG .NET Desktop Runtime was downloaded!"
         [ ! -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && echo -e " $ERROR_LOG .NET Desktop Runtime was not downloaded." && exit
-        "$RUNNER_EXEC" "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart &> /dev/null && echo -e " $DONE_LOG .NET Runtime installed!"
-        "$RUNNER_EXEC" "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart &> /dev/null && echo -e " $DONE_LOG .NET Desktop Runtime installed!"  
+        "$RUNNER_EXEC" "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart &>> $AR_LOG && echo -e " $DONE_LOG .NET Runtime installed!"
+        "$RUNNER_EXEC" "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart &>> $AR_LOG && echo -e " $DONE_LOG .NET Desktop Runtime installed!"  
     (
         echo -e "$WAIT_LOG Downloading Teknoparrot (Web-Installer)"
-        wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP" &> /dev/null
+        wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP" &>> $AR_LOG
             [ -f "$TMP"/TPBootstrapper.zip ] && echo -e " $DONE_LOG TPBootstrapper was downloaded!"
             [ ! -f "$TMP"/TPBootstrapper.zip ] && echo -e " $ERROR_LOG TPBootstrapper was not downloaded." && exit
-        unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM" &> /dev/null
+        unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM" &>> $AR_LOG
             [ -f "$PROGRAM"/TPBootstrapper.exe ] && echo -e " $DONE_LOG TPBootstrapper was extracted!"
             [ ! -f "$PROGRAM"/TPBootstrapper.exe ] && echo -e " $ERROR_LOG TPBootstrapper was not extracted." && exit
         cd "$PROGRAM"
         echo -e "$WAIT_LOG Installing Teknoparrot (Web-Installer)"
             [ -f "$PROGRAM"/TPBootstrapper.exe ] && echo -e " $DONE_LOG TPBootstrapper was found!"
             [ ! -f "$PROGRAM"/TPBootstrapper.exe ] && echo -e " $ERROR_LOG TPBootstrapper was not found." && exit
-            "$RUNNER_EXEC" TPBootstrapper.exe &> /dev/null
+            "$RUNNER_EXEC" TPBootstrapper.exe &>> $AR_LOG
             [ -f "$PROGRAM"/TeknoParrotUi.exe ] && echo -e " $DONE_LOG Teknoparrot installed!"
             [ ! -f "$PROGRAM"/TeknoParrotUi.exe ] && echo -e " $ERROR_LOG Teknoparrot not installed." && exit
     )
+    ls $TREE && ls $TREE/* &>> $AR_LOG
     rm -rf "$PROGRAM"/TPBootstrapper*
     rm -rf "$TMP" && echo -e " $DONE_LOG Temporary files cleared!"
 }
@@ -105,7 +107,8 @@ executableCreation(){
 }
 
 ARL_NAME="Arcade Runtime Linux"
-ARL_VERSION="3.1-2"
+ARL_VERSION="3.1-3_tmp"
+AR_LOG=$HOME/AR.LOG
 DONE_LOG="\e[1;32m* [ DONE ]\033[0m"
 WAIT_LOG="\e[1;33m* [ WAIT ]\033[0m"
 ERROR_LOG="\e[1;31m* [ ERROR ]\033[0m"
