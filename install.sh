a3c1a0fc573cf40a6a1516aace4dbd0f0dc8cc09
sakaki <91630610+sakaki91@users.noreply.github.com>
2026-04-19 01:42:44 -0300

    Update 3.2-1
    An internal auto-update system (basic git pull) has been added, and I am currently bringing back (gradually and currently incomplete) umu-proton with umu-run (local) for specific games like WMMT, while the vast majority will continue via wine. Currently this is incomplete, but in the future there will be a double prefix system (precisely so that one runner does not influence the other in case of switching). The project development has resumed, but at a slower pace, for obvious reasons: stress, fatigue, and mainly lack of motivation.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hostDependencyChecker(){
+primaryDependencyChecker(){
     dependencies=(wine bash wget unzip tar zenity)
     for cmd in "${dependencies[@]}"; do
         if ! command -v "$cmd" >/dev/null 2>&1; then
@@ -10,20 +10,21 @@ hostDependencyChecker(){
 }
 
 atomicTree(){
-    TMP=${TREE}/PREFIX/drive_c/TMP
-    PROGRAM=${TREE}/PROGRAM
-    PREFIX=${TREE}/PREFIX
-    [ -f "$TREE"/TeknoParrot ] && rm -rf "$TREE"/TeknoParrot
+    PROGRAM=${TREE}/bin
+    PREFIX=${TREE}/pfx
+    TMP=${PREFIX}/drive_c/tmp
+    [ -f "$TREE"/awl ] && rm -rf "$TREE"/awl
     [ -d "$PROGRAM" ] && rm -rf "$PROGRAM"
     [ -d "$PREFIX" ] && rm -rf "$PREFIX"
     mv "$TREE" "$TREE".old
-    mkdir -p "$TREE"/PROGRAM && mkdir -p "$PREFIX/drive_c/TMP"
+    mkdir -p "$TREE"/{bin,pfx} && mkdir -p "$PREFIX/drive_c/tmp"
+    #sed -i "s|^\s*AWL_LOCATION=.*|AWL_LOCATION=\"$TREE\"|" awl.sh
 }
 
 dependencyInstall(){
     clear
     export WINEPREFIX=${PREFIX}
-    printf "$NORMAL_LOG Wineboot. ($PREFIX)" && wineboot -u &> $AWL_LOG
+    printf "$NORMAL_LOG Wineboot. ($WINEPREFIX)" && wineboot -u &> $AWL_LOG
     if [ -d "$PREFIX"/drive_c ]; then
         printf " $DONE_LOG\n$NORMAL_LOG Downloading dependencies."
         wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
@@ -63,6 +64,7 @@ dependencyInstall(){
         ls $TREE &>> $AWL_LOG && ls $TREE/* &>> $AWL_LOG
         rm -rf "$PROGRAM"/TPBootstrapper*
         rm -rf "$TMP" && printf "$NORMAL_LOG Temporary files cleared. $DONE_LOG\n"
+        [ $UMU == "true" ] && printf "add export PATH="$HOME/.local/bin:$PATH" to your ~/.bashrc\n"
     else
         printf " $ERROR_LOG\n" && exit
     fi
@@ -73,27 +75,31 @@ executableCreation(){
         cd "$TREE"
         HEADER="#!/bin/bash"
         EXEC="LC_ALL=en_US.UTF-8 LC_NUMERIC=en_US.UTF-8 LANG=en_US.UTF-8 WINEPREFIX=$PREFIX wine $PROGRAM/TeknoParrotUi.exe"
-        echo $HEADER > TeknoParrot
-        echo $EXEC >> TeknoParrot
-        chmod +x TeknoParrot
+        echo $HEADER > awl
+        echo $EXEC >> awl
+        chmod +x awl
     )
 }
 
-AWL_NAME="Arcade Wrapper Linux/Unix-like" && AWL_VERSION="3.2" && AWL_LOG="/dev/null"
+AWL_NAME="Arcade Wrapper Linux" && AWL_VERSION="3.2-1" && AWL_LOG="/dev/null"
 DEFAULT_COLOR="\033[0m" && GRAY_COLOR="\e[0;37m" && BLACK_BG="\e[0;40m" && DONE_LOG="\e[1;32mOK\033[0m" && NORMAL_LOG="\e[1;34m*\033[0m" && ERROR_LOG="\e[1;31mERROR\033[0m"
 TREE=${HOME}/TeknoParrot
 DXVK_VERSION="2.7.1"
+UMU="false"
 
 case $1 in
     "--help")
         echo -e "\n$AWL_NAME $AWL_VERSION\n\n--help\t\tShow this message.\n--version\tShow wrapper version.\n\n$BLACK_BG./install @args @subargs$DEFAULT_COLOR\n\n@args:\n--debug\t\tThis executes the script and generates a log file (AWL.LOG) in $HOME.\n--last-dir\tUses the last directory used in the script (ideal for reinstalls/maintenance).\n--custom-dir\tWith this flag you can choose a custom installation directory.\n\n$GRAY_COLOR@subargs:\n-watch\t\tShows the last directory used, without installing anything.\n-debug \t\tThe -debug option serves as an additional subargument, for debug in the default directory, simply use --debug.\n$DEFAULT_COLOR\n"
         exit
     ;;
-    "--last-dir")
-        source src/lastpath
-        [[ $2 == "-watch" ]] && echo -e "Last path used: $LASTPATH" && exit
-        [[ $2 == "-debug" ]] && AWL_LOG=$HOME/awl.log
-        TREE="$LASTPATH"
+    "--umu")
+    (
+        git clone https://github.com/Open-Wine-Components/umu-launcher
+        cd umu-launcher
+        ./configure.sh --user-install
+        make install
+        UMU="true"
+    )
     ;;
     "--custom-dir")
         TREE=$(zenity --file-selection --directory --title "Select your desired directory:")/TeknoParrot
@@ -108,10 +114,8 @@ case $1 in
     ;;
 esac
 
-FINAL_PATH="$TREE"
-mkdir -p src/ && echo LASTPATH=$FINAL_PATH > src/lastpath
-hostDependencyChecker
-clear
+primaryDependencyChecker
+git pull
 atomicTree
 dependencyInstall
 executableCreation
